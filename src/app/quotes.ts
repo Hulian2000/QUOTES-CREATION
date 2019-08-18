5d09c3007d4f6fef4a4102e1560df48bc1325906
Hulian2000 <prepmoringa11@gmail.com>
2019-08-18 12:08:27 +0300

    add false and zero

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,10 @@
 export class Quotes {
-    
+    ShowDetails:boolean;
+    upvotes:number;
+    downvotes:number;
     constructor(public id:number,public name:string,public author:string,public submittedBy:string,public datecreation:Date){
-
+        this.ShowDetails=false;
+        this.upvotes=0;
+        this.downvotes=0;
     }
 }
